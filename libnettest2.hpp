42f8e7699fd1f9ba3090b0a764969381dc4d82fe
Simone Basso <bassosimone@gmail.com>
2018-09-11 16:48:00 +0200

    Bless libnettest2 v0.1.0
    I think I have implemented enough functionality to call this a
    good first version with which I can now try to experiment directly
    by integrating it inside of MK.

--- libnettest2.hpp
+++ libnettest2.hpp
@@ -73,7 +73,7 @@ using Version = unsigned int;
 constexpr Version version_major = Version{0};
 
 /// Minor API version number of measurement-kit/libnettest2.
-constexpr Version version_minor = Version{0};
+constexpr Version version_minor = Version{1};
 
 /// Patch API version number of measurement-kit/libnettest2.
 constexpr Version version_patch = Version{0};
