f72700fcf12a8417fbb526ee536e830f89fcd598
Simone Basso <bassosimone@gmail.com>
2018-09-12 19:29:20 +0200

    Bump minor because we changed a method name

--- libnettest2.hpp
+++ libnettest2.hpp
@@ -73,7 +73,7 @@ using Version = unsigned int;
 constexpr Version version_major = Version{0};
 
 /// Minor API version number of measurement-kit/libnettest2.
-constexpr Version version_minor = Version{2};
+constexpr Version version_minor = Version{3};
 
 /// Patch API version number of measurement-kit/libnettest2.
 constexpr Version version_patch = Version{0};
